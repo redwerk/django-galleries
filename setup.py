6db7bb342d1da10d292bf2b76003c7f98c9e2832
mohamed-ali-affes <mohamed.ali.affes@gmail.com>
2014-12-16 08:03:40 +0100

    Editing dependencies

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@ from setuptools import setup
 execfile('galleries/version.py')
 
 setup(
-    name = 'django-galleries',
-    version = __version__,
-    description = "Django Galleries is a photo gallery app with support for cropping.",
-    long_description = open('README.md').read(),
-    author = "Espen Høgbakk",
-    author_email = "espen@hogbakk.no",
-    url = "http://github.com/espenhogbakk/django-galleries",
-    packages = ['galleries'],
+    name='django-galleries',
+    version=__version__,
+    description="Django Galleries is a photo gallery app with support for cropping.",
+    long_description=open('README.md').read(),
+    author="Espen Høgbakk",
+    author_email="espen@hogbakk.no",
+    url="http://github.com/espenhogbakk/django-galleries",
+    packages=['galleries'],
     include_package_data = True,
     install_requires = [
         'easy_thumbnails', 
         'django-respite>=1.2.0',
-        'easy_thumbnails',
-        'PIL',
+        'Pillow',
         'django-orderable',
         'django-image-cropping'
     ],

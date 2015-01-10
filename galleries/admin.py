ce52e1d712236c63c2e2cbb6ad4c879e1e2523cc
mohamed-ali-affes <mohamed.ali.affes@gmail.com>
2015-01-10 11:22:20 +0100

    Hiding sortable field in admin change form if grappelli is installed

--- galleries/admin.py
+++ galleries/admin.py
@@ -1,16 +1,29 @@
+from django.conf import settings
 from django.contrib import admin
-
+from django.forms.widgets import HiddenInput
 from orderable.admin import OrderableTabularInline
 
 from models import Gallery, Image
 
-class ImageInline(OrderableTabularInline):
-    model = Image
-    extra = 0
+
+class SortableMixin(object):
+	sortable_field_name = "sort_order"
+
+	def formfield_for_dbfield(self, db_field, **kwargs):
+		if 'grappelli' in settings.INSTALLED_APPS:
+			if db_field.name == self.sortable_field_name:
+				kwargs["widget"] = HiddenInput()
+		return super(SortableMixin, self).formfield_for_dbfield(db_field, **kwargs)
+
+
+class ImageInline(SortableMixin, OrderableTabularInline):
+	model = Image
+	extra = 0
+
 
 class GalleryAdmin(admin.ModelAdmin):
-    inlines = [
-        ImageInline
-    ]
+	inlines = [
+		ImageInline
+	]
 
 admin.site.register(Gallery, GalleryAdmin)
